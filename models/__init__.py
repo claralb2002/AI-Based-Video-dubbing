bf0957c6956190bc79376350d1ffc7477a33689c
claralb2002 <s224189@dtu.dk>
2025-06-05 10:44:30 +0200

    Makes updates to init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # models/__init__.py
 from .whisper_transcriber import WhisperTranscriber
-from .translator import Translator  # If you have a translation model
-from .text_to_speech import TextToSpeech  # If you have a TTS model
+from .translator import ChunkTranslator  
+# from .text_to_speech import TextToSpeech 
 
-__all__ = ["WhisperTranscriber"]  # ["WhisperTranscriber", "Translator", "TextToSpeech"]
+__all__ = ["WhisperTranscriber, ChunkTranslator"]  
